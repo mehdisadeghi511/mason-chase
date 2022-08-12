1df34109c344ac95e3bbba520f9b23bd8dc4f89d
mehdi sadeghi <mehdisadeghi511@gmail.com>
2022-08-13 02:10:04 +0430

    final

--- src/app/pages/auth-pages/login/login.component.spec.ts
+++ src/app/pages/auth-pages/login/login.component.spec.ts
@@ -1,4 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { LoginComponent } from './login.component';
 
@@ -8,9 +10,9 @@ describe('LoginComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
-    })
-    .compileComponents();
+      declarations: [LoginComponent],
+      imports: [RouterTestingModule, , FormsModule, ReactiveFormsModule],
+    }).compileComponents();
   });
 
   beforeEach(() => {
